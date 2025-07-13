dbb7d8c90c51ac700e1a9d3ee299b47cefcd533e
endo-ava <ryuto9007@gmail.com>
2025-07-13 23:40:14 +0900

    fix: delete useIsMobile

--- src/features/view-controller/index.ts
+++ src/features/view-controller/index.ts
@@ -2,5 +2,4 @@ export { ViewController } from './components/ViewController';
 export { HubRadioGroup } from './components/HubRadioGroup';
 export { HubOptionButton } from './components/HubOptionButton';
 export { useViewController } from './hooks/useViewController';
-export { useIsMobile } from './hooks/useIsMobile';
 export type { ViewControllerProps } from './components/ViewController';
